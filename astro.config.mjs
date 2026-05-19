b5d2d49063509abca4cb7bbe07091ef1b15e15df
Jiminy Panoz <JayPanoz@users.noreply.github.com>
2026-05-19 10:35:08 +0200

    Build + deploy action

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,8 @@ import { defineConfig } from "astro/config";
 import { defaultLocale, locales } from "./src/i18n/locales.js";
 
 export default defineConfig({
+  site: "https://edrlab.github.io",
+  base: "/thorium-reader-website",
   output: "static",
   i18n: {
     defaultLocale,
