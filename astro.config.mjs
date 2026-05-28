6a129b98ff11797d89ce8c797926c2c9fd196ef6
Jiminy Panoz <JayPanoz@users.noreply.github.com>
2026-05-28 13:39:27 +0200

    Add sitemap (#18)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,19 @@
 import { defineConfig } from "astro/config";
-import { defaultLocale, locales } from "./src/i18n/locales.js";
+import sitemap from "@astrojs/sitemap";
+import { defaultLocale, locales, bcp47Locale } from "./src/i18n/locales.js";
 
 export default defineConfig({
   site: "https://edrlab.github.io",
   base: "/thorium-reader-website/",
   output: "static",
+  integrations: [
+    sitemap({
+      i18n: {
+        defaultLocale,
+        locales: Object.fromEntries(locales.map((l) => [l, bcp47Locale[l]])),
+      },
+    }),
+  ],
   i18n: {
     defaultLocale,
     locales,
