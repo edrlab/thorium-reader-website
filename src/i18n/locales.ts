36efede55e4c88c7cba56f6825f3dc2b70b5fe33
Jiminy Panoz <JayPanoz@users.noreply.github.com>
2026-08-26 15:05:12 +0200

    Add spanish to supported locales

--- src/i18n/locales.ts
+++ src/i18n/locales.ts
@@ -1,9 +1,10 @@
 export const defaultLocale = "en";
-export const locales = ["en", "fr", "it"] as const;
+export const locales = ["en", "fr", "it", "es"] as const;
 export type Locale = (typeof locales)[number];
 
 export const bcp47Locale: Record<Locale, string> = {
   en: "en-us",
   fr: "fr-fr",
   it: "it-it",
+  es: "es-es",
 };
